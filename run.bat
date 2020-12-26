e4f5358d586d32f7d5e55a9582eb2e608f16f7ae
anborg <dev@gmail.com>
2020-12-26 18:57:12 -0500

    netlify settings

--- run.bat
+++ run.bat
@@ -1,2 +1,11 @@
-set ELEVENTY_EXPERIMENTAL=true
-npx @11ty/eleventy --serve
+
+
+::set ELEVENTY_EXPERIMENTAL=true
+::npx @11ty/eleventy --serve
+
+::    "start": "run-p start:**",
+::    "start:eleventy": "ELEVENTY_EXPERIMENTAL=true npx @11ty/eleventy --serve --port=3000",
+::    "start:netlifyProxy": "npx netlify-cms-proxy-server",
+
+
+npm start
